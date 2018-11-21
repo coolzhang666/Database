2eb2fd0cf0f40493f24d85783f0843c89cd46066
coolzhang666 <2227603597@qq.com>
2018-11-21 13:11:25 +0800

    update

--- Exercise/exercise3/exercise3.sql
+++ Exercise/exercise3/exercise3.sql
@@ -37,27 +37,153 @@ where Sdept = 'CS' and Sage = (
 
 
 -- 6.找出各系年龄最大的学生，显示其学号、姓名
-
+select Sno, Sname
+from S x
+where Sage = (
+				select MAX(Sage)
+				from S y
+				where y.Sdept = x.Sdept
+			 )
 
 
 -- 7.统计“CS”系学生的人数
+select Sdept, count(*) 人数
+from S
+where Sdept = 'CS'
+group by Sdept;
+
+
 -- 8.统计各系学生的人数，结果按升序排列
+select Sdept, count(Sdept) 人数
+from S
+group by Sdept
+order by count(Sdept) ASC
+
+
 -- 9.按系统计各系学生的平均年龄，结果按降序排列
+select Sdept, avg(Sage)
+from S
+group by Sdept
+order by avg(Sage) DESC
+
+
 -- 10.查询每门课程的课程名
--- 11.查询无先修课的课程的课程名和学时数
--- 12.统计无先修课的课程的学时总数
+select Cname
+from C
+
+
+-- 11.查询无先修课的课程的课程名和学分数
+select Cname, Ccredit
+from C
+where Cpno is null
+
+
+-- 12.统计无先修课的课程的学分总数
+select sum(Ccredit) 无先修课程学分总数
+from C
+where Cpno is null
+
+
 -- 13.统计每位学生选修课程的门数、学分及其平均成绩
+select Sno, count(C.Cno) 选修课程门数, sum(C.Ccredit) 选修学分数, avg(SC.Grade) 选修平均成绩
+from SC, C
+where SC.Cno = C.Cno
+group by Sno
+
+
 -- 14.统计选修每门课程的学生人数及各门课程的平均成绩
+select Cno, count(Sno) 选修人数, avg(SC.Grade) 平均成绩
+from SC
+group by Cno
+
+
 -- 15.找出平均成绩在85分以上的学生，结果按系分组，并按平均成绩的升序排列
+select Sdept, SC.Sno, avg(Grade) 平均成绩
+from S, SC
+where SC.Sno = S.Sno
+group by Sdept, SC.Sno having avg(Grade) >= 85
+order by avg(Grade) ASC
+
+
+
 -- 16.查询选修了“1”或“2”号课程的学生学号和姓名
+select Sno, Sname
+from S
+where Sno in (
+				select Sno
+				from SC
+				where Cno = '1' or Cno = '2'
+			 )
+
+
 -- 17.查询选修了“1”和“2”号课程的学生学号和姓名
+select Sno, Sname
+from S
+where Sno in (
+				select X.Sno
+				from SC X, SC Y
+				where X.Cno = '1' and Y.Cno = '2' and X.Sno = Y.Sno
+			 )
+
+
 -- 18.查询选修了课程名为“数据库系统”且成绩在60分以下的学生的学号、姓名和成绩
+select SC.Sno, Sname, Grade
+from S, SC
+where SC.Sno = S.Sno and Grade < 60 and Cno in (
+												select Cno
+												from C
+												where Cname = '数据库'
+											   )
+update SC
+set Grade = 92
+where Sno = '201215121' and Cno = '1'
+
+
 -- 19.查询每位学生选修了课程的学生信息（显示：学号，姓名，课程号，课程名，成绩）
+select SC.Sno, Sname, C.Cno, Cname, Grade
+from S, C, SC
+where SC.Sno = S.Sno and SC.Cno = C.Cno
+
+
 -- 20.查询没有选修课程的学生的基本信息
+select *
+from S
+where Sno not in (
+					select Sno
+					from SC
+				 )
+
+delete from SC
+where Sno = '201215125'
+
+
 -- 21.查询选修了3门以上课程的学生学号
+select Sno
+from SC
+group by Sno
+having count(Cno) >= 3
+
+
 -- 22.查询选修课程成绩至少有一门在80分以上的学生学号
+select distinct Sno
+from SC
+where Grade > 80
+
+
 -- 23.查询选修课程成绩均在80分以上的学生学号
+select Sno
+from SC
+group by Sno
+having min(Grade) > 80
+
+
 -- 24.查询选修课程平均成绩在80分以上的学生学号
+select Sno
+from SC
+group by Sno
+having avg(Grade) > 80
+
+
 select * from S;
 select * from C;
 select * from SC;
--- Exercise/exercise4/exercise4.sql
+++ Exercise/exercise4/exercise4.sql
@@ -2,14 +2,93 @@ use [11603990329_章强];
 
 
 -- 1.定义“IS”系学生基本情况视图V_IS 并查询结果
+create view V_IS
+as
+select *
+from S
+where Sdept = 'IS'
+with check option
+
+select * from V_IS
+
+
 -- 2.将S，C，SC表中学生的学号，姓名，课程号，课程名，成绩定义为视图V_S_C_G并查询结果
+create view V_S_C_G
+as
+select SC.Sno, Sname, SC.Cno, Cname, Grade
+from S, C, SC
+where SC.Sno = S.Sno and SC.Cno = C.Cno
+with check option
+
+select * from V_S_C_G
+
+
 -- 3.将各系学生人数，平均年龄定义为视图V_NUM_AVG并查询结果
+create view V_NUM_AVG(Sdept, Num, Avg_age)
+as
+select Sdept, count(Sdept), avg(Sage)
+from S
+group by Sdept
+
+select * from V_NUM_AVG
+
+
 -- 4.定义一个反映学生出生年份的视图V_YEAR并查询结果
+create view V_YEAR(Sno, Syear)
+as
+select Sno, 2018-Sage
+from S
+
+select * from V_YEAR
+
+
 -- 5.将各位学生选修课程的门数及平均成绩定义为视图V_AVG_S_G并查询结果
+create view V_AVG_S_G(Sno, count_c, avg_grade)
+as
+select Sno, count(Cno), avg(Grade)
+from SC
+group by Sno
+
+select * from V_AVG_S_G
+
+
 -- 6.将各门课程的选修人数及平均成绩定义为视图V_AVG_C_G并查询结果
+create view V_AVG_C_G(Cno, count_s, avg_grage)
+as
+select Cno, count(Sno), avg(Grade)
+from SC
+group by Cno
+
+select * from V_AVG_C_G
+
+
 -- 7.查询平均成绩为90分以上的学生学号、姓名和成绩
+select Sno, Sname, Grade
+from V_S_C_G
+where Sno in (
+				select Sno
+				from V_S_C_G
+				group by Sno
+				having avg(Grade) >= 90
+			 )
+
+
 -- 8.查询各课成绩均大于平均成绩的学生学号、姓名、课程和成绩
+select Cno, avg(Grade) -- 各科平均成绩
+from SC
+group by Cno
+
+
 -- 9.按系统计各系平均成绩在80分以上的人数，结果按降序排列
+select Sdept, count(SC.Sno)
+from SC, S
+where SC.Sno = S.Sno
+group by Sdept
+having avg(Grade) > 80
+order by avg(Grade) DESC
+
+
+
 -- 10.通过视图V_IS，分别将学号为“S1”和“S4”的学生姓名更改为“S1_MMM”,”S4_MMM” 并查询结果
 -- 11.通过视图V_IS，新增加一个学生记录 ('S12','YAN XI',19,'IS')，并查询结果
 -- 12.通过视图V_IS，新增加一个学生记录 ('S13','YAN XI',19,'MA')，并查询结果
